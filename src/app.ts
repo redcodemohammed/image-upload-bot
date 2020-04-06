1a9b54a30cc7333437de6f7f0cbeee879195ff4a
redcodemohammed <redcodemohammed@gmail.com>
2020-04-06 10:32:06 +0300

    turned the bot to typescript

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-const telegraf = require("telegraf");
-const axios = require("axios");
-const http = require("http");
-const server = http.createServer((req, res) => res.end("Bot is running, enjoy!"));
+import telegraf from "telegraf";
+import axios from "axios";
+import http from "http";
 
-const api_home = "https://api.imgbb.com/1/upload";
-const api_key = "8b9829fa45abad3ccbf76d25ab2b3af3";
-const botToken = "716496504:AAEgHUn_g2luL53Vf7dEB29bCGUwj5uFHUQ";
+const api_home = process.env.api;
+const api_key = process.env.apiKey;
+const botToken = process.env.token;
 const port = process.env.PORT;
 
 const bot = new telegraf(botToken);
@@ -13,6 +12,7 @@ const bot = new telegraf(botToken);
 bot.start(ctx => {
     ctx.reply(`مرحبا, عند ارسال صورة تأكد من ارسالها كملف لكي لا تقل دقتها`);
 });
+
 
 bot.on("document", async ctx => {
     if (!(/\.(gif|jpe?g|tiff|png|webp|bmp)$/i).test(ctx.message.document.file_name)) {
@@ -27,7 +27,7 @@ bot.on("document", async ctx => {
     let api = `${api_home}?key=${api_key}&name=${name}&image=${link}`;
     try {
 
-        let upload = await axios.default.get(api);
+        let upload = await axios.get(api);
         let imageLink = upload.data.data.image.url;
         ctx.reply(`تم رفع الصورة على الرابط ${imageLink}.`);
     } catch (err) {
@@ -39,4 +39,6 @@ bot.on("photo", ctx => {
     ctx.reply(`ارسل الصورة كملف لكي لا يتم ضغطها`);
 });
 bot.launch();
+
+const server = http.createServer((req, res) => res.end("Bot is running, enjoy!"));
 server.listen(port);
